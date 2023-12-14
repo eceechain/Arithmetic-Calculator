74e587a99eeb2b885b0dbf643e87196b15ef2c10
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-12-14 14:55:01 +0300

    completed task

---
+++ lib/calculator.py
@@ -0,0 +1,43 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError("Division by zero is not allowed")
+    return x / y
+
+def calculator():
+    while True:
+        try:
+            num1 = float(input("Enter first number: "))
+            operator = input("Enter operator (+, -, *, /): ")
+            num2 = float(input("Enter second number: "))
+
+            if operator == '+':
+                result = add(num1, num2)
+            elif operator == '-':
+                result = subtract(num1, num2)
+            elif operator == '*':
+                result = multiply(num1, num2)
+            elif operator == '/':
+                result = divide(num1, num2)
+            else:
+                print("Invalid operator")
+                continue
+
+            print(f"Result: {result}")
+            break
+
+        except ValueError:
+            print("Please enter a valid number")
+        except ZeroDivisionError as e:
+            print(e)
+            
+if __name__ == "__main__":
+    calculator()
