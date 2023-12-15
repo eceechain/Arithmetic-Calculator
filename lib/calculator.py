714ce4ee3a4a4f69bb5ce41a100101f47301bb91
Tracy <tracy.kagiri@moringa.student.com>
2023-12-15 14:56:10 +0300

    commiting

--- lib/calculator.py
+++ lib/calculator.py
@@ -1,134 +1,68 @@
 from tkinter import *
 
-def button_press (num):
-    
-     global equation_text
-
-     equation_text = equation_text + str(num)
-
-     equation_label.set(equation_text)
-
-
+def button_press(num):
+    global equation_text
+    equation_text = equation_text + str(num)
+    equation_label.set(equation_text)
 
 def equals():
-    
-      global equation_text
-
-      try:
-
+    global equation_text
+    try:
         total = str(eval(equation_text))
-
         equation_label.set(total)
-
         equation_text = total
-
-      except SyntaxError:
-        equation_label.set("syntax error")
-
+    except SyntaxError:
+        equation_label.set("Syntax error")
         equation_text = ""
-
-      except ZeroDivisionError:
-
-        equation_label.set("arithmetic error")
-
+    except ZeroDivisionError:
+        equation_label.set("Arithmetic error")
         equation_text = ""
-
 
 def clear():
-     
-    
-
-     global equation_text
-
-     equation_label.set("")
-
-     equation_text = ""
-
+    global equation_text
+    equation_label.set("")
+    equation_text = ""
 
 window = Tk()
 window.title("Calculator program")
-window.geometry("500x500")
+window.geometry("500x600")
+window.configure(bg="#f2f2f2") 
 
 equation_text = ""
 
 equation_label = StringVar()
 
-label = Label(window, textvariable=equation_label, font=('consolas' ,20), bg="white", width=24, height=2)
+label = Label(window, textvariable=equation_label, font=('consolas', 20), bg="white", width=24, height=2)
 label.pack()
 
-frame = Frame(window)
+frame = Frame(window, bg="#666666") 
 frame.pack()
 
-button1 = Button(frame, text=1, height=4, width=9, font=35,
-                 command=lambda: button_press(1))
-button1.grid(row=0, column=0)
+button_font = ('Arial', 18, 'bold')
 
-button2 = Button(frame, text=2, height=4, width=9, font=35,
-                 command=lambda: button_press(2))
-button2.grid(row=0, column=1)
+buttons = [
+    Button(frame, text=str(i), height=4, width=9, font=button_font, command=lambda i=i: button_press(i))
+    for i in range(10)
+]
 
-button3 = Button(frame, text=3, height=4, width=9, font=35,
-                 command=lambda: button_press(3))
-button3.grid(row=0, column=2)
+for i, button in enumerate(buttons[:9]):
+    button.grid(row=i // 3, column=i % 3)
 
-button4 = Button(frame, text=4, height=4, width=9, font=35,
-                 command=lambda: button_press(4))
-button4.grid(row=1, column=0)
+buttons[0].grid(row=3, column=1)
 
-button5 = Button(frame, text=5, height=4, width=9, font=35,
-                 command=lambda: button_press(5))
-button5.grid(row=1, column=1)
+operators = ['+', '-', 'x', '/']
+operator_buttons = [
+    Button(frame, text=op, height=4, width=9, font=button_font, command=lambda op=op: button_press(op))
+    for op in operators
+]
 
-button6 = Button(frame, text=6, height=4, width=9, font=35,
-                 command=lambda: button_press(6))
-button6.grid(row=1, column=2)
+for i, button in enumerate(operator_buttons):
+    button.grid(row=i, column=3)
 
-button7 = Button(frame, text=7, height=4, width=9, font=35,
-                 command=lambda: button_press(7))
-button7.grid(row=2, column=0)
+Button(frame, text='=', height=4, width=9, font=button_font, command=equals).grid(row=3, column=2)
+Button(frame, text='.', height=4, width=9, font=button_font, command=lambda: button_press('.')).grid(row=3, column=0)
 
-button8 = Button(frame, text=8, height=4, width=9, font=35,
-                 command=lambda: button_press(8))
-button8.grid(row=2, column=1)
+clear_button = Button(window, text='Clear', height=4, width=15, font=button_font, command=clear)
+clear_button.pack()
 
-button9 = Button(frame, text=9, height=4, width=9, font=35,
-                 command=lambda: button_press(9))
-button9.grid(row=2, column=2)
-
-button0 = Button(frame, text=0, height=4, width=9, font=35,
-                 command=lambda: button_press(0))
-button0.grid(row=3, column=1)
-
-plus = Button(frame, text='+', height=4, width=9, font=35,
-                 command=lambda: button_press('+'))
-plus.grid(row=0, column=3)
-
-minus = Button(frame, text='-', height=4, width=9, font=35,
-                 command=lambda: button_press('-'))
-minus.grid(row=1, column=3)
-
-multiply = Button(frame, text='x', height=4, width=9, font=35,
-                 command=lambda: button_press('x'))
-multiply.grid(row=2, column=3)
-
-divide = Button(frame, text='/', height=4, width=9, font=35,
-                 command=lambda: button_press('/'))
-divide.grid(row=3, column=3)
-
-equal = Button(frame, text='=', height=4, width=9, font=35,
-                 command=lambda: button_press('='))
-equal.grid(row=3, column=2)
-
-decimal = Button(frame, text='.', height=4, width=9, font=35,
-                 command=lambda: button_press('.'))
-decimal.grid(row=3, column=0)
-
-clear = Button(window, text='clear', height=4, width=15, font=35,
-                 command=clear)
-
-clear.pack()
 window.mainloop()
-
-
-
-
