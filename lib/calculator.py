65a298684be26ba6d9d9841afef7e30920dbc4f0
Tracy <tracy.kagiri@moringa.student.com>
2023-12-15 22:10:45 +0300

    completed

--- lib/calculator.py
+++ lib/calculator.py
@@ -1,8 +1,9 @@
 from tkinter import *
+from tkinter import ttk
 
-def button_press(num):
+def button_press(value):
     global equation_text
-    equation_text = equation_text + str(num)
+    equation_text = equation_text + str(value)
     equation_label.set(equation_text)
 
 def equals():
@@ -11,11 +12,8 @@ def equals():
         total = str(eval(equation_text))
         equation_label.set(total)
         equation_text = total
-    except SyntaxError:
-        equation_label.set("Syntax error")
-        equation_text = ""
-    except ZeroDivisionError:
-        equation_label.set("Arithmetic error")
+    except (SyntaxError, ZeroDivisionError):
+        equation_label.set("Error")
         equation_text = ""
 
 def clear():
@@ -23,46 +21,59 @@ def clear():
     equation_label.set("")
     equation_text = ""
 
+def delete():
+    global equation_text
+    equation_text = equation_text[:-1]
+    equation_label.set(equation_text)
+
 window = Tk()
 window.title("Calculator program")
 window.geometry("500x600")
-window.configure(bg="#f2f2f2") 
+window.configure(bg="black")  
 
 equation_text = ""
 
 equation_label = StringVar()
 
-label = Label(window, textvariable=equation_label, font=('consolas', 20), bg="white", width=24, height=2)
+label = Label(window, textvariable=equation_label, font=('consolas', 30), bg="black", fg="white", width=24, height=2)
 label.pack()
 
-frame = Frame(window, bg="#666666") 
+frame = Frame(window, bg="black")  
 frame.pack()
 
-button_font = ('Arial', 18, 'bold')
+
+button_font = ('Arial', 32, 'bold')
+
+style = ttk.Style()
+style.configure("DarkBlue.TButton", foreground="white", background="#001f3f", font=button_font, padding=(40, 40)) 
 
 buttons = [
-    Button(frame, text=str(i), height=4, width=9, font=button_font, command=lambda i=i: button_press(i))
+    ttk.Button(frame, text=str(i), style="DarkBlue.TButton", command=lambda i=i: button_press(i))
     for i in range(10)
 ]
 
+
 for i, button in enumerate(buttons[:9]):
-    button.grid(row=i // 3, column=i % 3)
+    button.grid(row=i // 3, column=i % 3, padx=5, pady=5)
 
-buttons[0].grid(row=3, column=1)
 
-operators = ['+', '-', 'x', '/']
+buttons[0].grid(row=3, column=1, padx=5, pady=5)
+
+operators = ['+', '-', '*', '/']
 operator_buttons = [
-    Button(frame, text=op, height=4, width=9, font=button_font, command=lambda op=op: button_press(op))
+    ttk.Button(frame, text=op, style="DarkBlue.TButton", command=lambda op=op: button_press(op))
     for op in operators
 ]
 
 for i, button in enumerate(operator_buttons):
-    button.grid(row=i, column=3)
+    button.grid(row=i, column=3, padx=5, pady=5)
 
-Button(frame, text='=', height=4, width=9, font=button_font, command=equals).grid(row=3, column=2)
-Button(frame, text='.', height=4, width=9, font=button_font, command=lambda: button_press('.')).grid(row=3, column=0)
 
-clear_button = Button(window, text='Clear', height=4, width=15, font=button_font, command=clear)
-clear_button.pack()
+ttk.Button(frame, text='=', style="DarkBlue.TButton", command=equals).grid(row=3, column=2, padx=5, pady=5)
+ttk.Button(frame, text='.', style="DarkBlue.TButton", command=lambda: button_press('.')).grid(row=3, column=0, padx=5, pady=5)
+ttk.Button(frame, text='Delete', style="DarkBlue.TButton", command=delete).grid(row=0, column=0, padx=5, pady=5)
+
+clear_button = ttk.Button(window, text='Clear', style="DarkBlue.TButton", command=clear)
+clear_button.pack(pady=10)
 
 window.mainloop()
